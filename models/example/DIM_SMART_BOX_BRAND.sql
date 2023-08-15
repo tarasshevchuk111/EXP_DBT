124487497ca815112ca35dba63cda4b9a3a9aabb
Taras.Shevchuk@buyagift.com <Taras.Shevchuk@buyagift.com>
2023-08-15 09:03:40 +0000

    add source

--- models/example/DIM_SMART_BOX_BRAND.sql
+++ models/example/DIM_SMART_BOX_BRAND.sql
@@ -1,27 +1,9 @@
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='table') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+select 
+DIMSMARTBOXBRANDID AS DIM_SMART_BOXB_RAND_ID,
+SMARTBOXNATIVEID AS SMART_BOX_NATIVE_ID,
+SMARTBOXCODE AS SMART_BOX_CODE,
+SMARTBOXBRAND AS SMART_BOXB_RAND
+from {{ source('MSSQL_DW_DBO', 'DIMSMARTBOXBRAND') }}
